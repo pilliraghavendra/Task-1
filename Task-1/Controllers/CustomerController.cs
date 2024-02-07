df9d4cbfa45cfe69dde123c71906756ac8ba3180
pilliraghavendra <pilliraghavendra0610@gmail.com>
2024-02-07 12:22:53 +0530

    chang hp to asus

--- Task-1/Controllers/CustomerController.cs
+++ Task-1/Controllers/CustomerController.cs
@@ -16,7 +16,7 @@ namespace Task_1.Controllers
         public List<Product> ProductsDetails()
         {
             products.Add(new Product { Id = 1, Name = "Apple", Description = "Phone", Price = 80000 });
-            products.Add(new Product { Id = 2, Name = "Hp", Description = "Laptop", Price = 50000 });
+            products.Add(new Product { Id = 2, Name = "Asus", Description = "Laptop", Price = 50000 });
             products.Add(new Product { Id = 3, Name = "LG", Description = "Tv", Price = 40000 });
             products.Add(new Product { Id = 4, Name = "Dell", Description = "Laptop", Price = 70000 });
 
